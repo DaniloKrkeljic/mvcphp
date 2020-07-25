56f0ac4cc5b7a2f904d5acd7bad835ba8f041e88
= <=>
2020-07-25 15:20:01 +0200

    PHP MVC boiler

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+  // DB params
+  define('DB_HOST','localhost');
+  define('DB_USERNAME','root');
+  define('DB_PASSWORD','');
+  define('DB_NAME','mvcphp');
+
+  // App Root
+  define('APPROOT',dirname(dirname(__FILE__))); 
+  // URL Root
+  define('URLROOT','http://localhost/mvcphp');
+  // Site Name
+  define('SITENAME','MVCphp');
